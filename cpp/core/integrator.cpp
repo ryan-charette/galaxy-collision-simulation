93fa75f35c72c4fbfd22f564642a3bf68753adff
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2026-04-27 22:09:37 -0500

    Implemented range-based overloads

--- cpp/core/integrator.cpp
+++ cpp/core/integrator.cpp
@@ -1,5 +1,7 @@
 #include "core/integrator.hpp"
 
+#include <algorithm>
+
 namespace fmmgalaxy {
 
 void kick(std::vector<Particle>& particles, double dt) {
@@ -11,6 +13,20 @@ void kick(std::vector<Particle>& particles, double dt) {
 void drift(std::vector<Particle>& particles, double dt) {
     for (auto& particle : particles) {
         particle.position += particle.velocity * dt;
+    }
+}
+
+void kick(std::vector<Particle>& particles, std::size_t begin, std::size_t end, double dt) {
+    end = std::min(end, particles.size());
+    for (std::size_t i = begin; i < end; ++i) {
+        particles[i].velocity += particles[i].acceleration * dt;
+    }
+}
+
+void drift(std::vector<Particle>& particles, std::size_t begin, std::size_t end, double dt) {
+    end = std::min(end, particles.size());
+    for (std::size_t i = begin; i < end; ++i) {
+        particles[i].position += particles[i].velocity * dt;
     }
 }
 
