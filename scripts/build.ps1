6ef34c0088b0d00819a28e601e7bc183a13d561d
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2026-04-27 22:17:13 -0500

    Update build.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -2,6 +2,8 @@ $ErrorActionPreference = "Stop"
 
 $BuildDir = if ($env:BUILD_DIR) { $env:BUILD_DIR } else { "build" }
 $BuildType = if ($env:BUILD_TYPE) { $env:BUILD_TYPE } else { "Release" }
+$EnableMpi = if ($env:ENABLE_MPI) { $env:ENABLE_MPI } else { "ON" }
+$EnableCuda = if ($env:ENABLE_CUDA) { $env:ENABLE_CUDA } else { "ON" }
 
-cmake -S . -B $BuildDir -DCMAKE_BUILD_TYPE=$BuildType -DENABLE_MPI=OFF -DENABLE_CUDA=OFF
+cmake -S . -B $BuildDir -DCMAKE_BUILD_TYPE=$BuildType -DENABLE_MPI=$EnableMpi -DENABLE_CUDA=$EnableCuda
 cmake --build $BuildDir --config $BuildType
