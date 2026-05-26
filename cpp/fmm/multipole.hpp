ca77c5f6b137e767d2ee73c5d99afbf8045b17d4
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2026-05-26 09:48:03 -0500

    Add local expansion support for FMM translations

--- cpp/fmm/multipole.hpp
+++ cpp/fmm/multipole.hpp
@@ -13,7 +13,17 @@ struct CartesianMoments {
     MomentArray values{};
 };
 
+struct LocalExpansion {
+    Vec2 center{};
+    double radius{1.0};
+    int order{0};
+    MomentArray ax{};
+    MomentArray ay{};
+    MomentArray az{};
+};
+
 CartesianMoments zero_multipole_moments();
+LocalExpansion zero_local_expansion(const Vec2& center, double radius, int expansion_order);
 void add_multipole_point(CartesianMoments& moments, const Vec2& offset, double mass);
 void add_multipole_shifted_child(
     CartesianMoments& parent,
@@ -30,5 +40,17 @@ Vec2 multipole_acceleration(
     const PhysicsParams& params,
     int expansion_order
 );
+
+void add_multipole_to_local(
+    LocalExpansion& target,
+    const Vec2& source_center_of_mass,
+    double source_mass,
+    const CartesianMoments& source_moments,
+    const PhysicsParams& params
+);
+
+void add_local_to_local(LocalExpansion& target, const LocalExpansion& source);
+Vec2 evaluate_local_acceleration(const LocalExpansion& local, const Vec2& target_position);
+int normalize_expansion_order(int expansion_order);
 
 }  // namespace fmmgalaxy
